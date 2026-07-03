a55735dc766781434dd344640d51700707f5c74b
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-03 13:28:57 -0500

    Updaing the row using merge method

---
+++ src/main/java/com/telusko/app/LaunchUpdate.java
@@ -0,0 +1,46 @@
+package com.telusko.app;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.telusko.model.Student;
+
+public class LaunchUpdate {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = new Configuration().addAnnotatedClass(Student.class)
+				.configure().buildSessionFactory();
+		
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+		
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			Student student = new Student(102, "Raj Koothrapalli", 20, "koothrapallir@yahoo.com");
+			session.merge(student); // saveOrUpdate
+			flag = true;
+			
+		}
+		catch (HibernateException e) {
+			System.out.println(e);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			if(flag == true) {
+				transaction.commit();
+			}else {
+				transaction.rollback();
+			}
+		}
+
+	}
+
+}
